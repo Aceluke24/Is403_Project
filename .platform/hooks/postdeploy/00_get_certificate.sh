11a3ea6b3309342eeb7a9ac42e95c126feed8141
Aceluke24 <aceluke24@gmail.com>
2025-11-17 23:00:45 -0700

    y

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 # .platform/hooks/postdeploy/00_get_certificate.sh
-sudo certbot certonly --standalone -n \
-    -d is404inclass-env.eba-dgw88jm4.us-east-1.elasticbeanstalk.com \
-    --agree-tos \
-    --email lukeh0@byu.edu
-
-exit 0
+sudo certbot -n -d http://is404inclass-env.eba-dgw88jm4.us-east-1.elasticbeanstalk.com/ --nginx --agree-tos --email lukeh0@byu.edu
